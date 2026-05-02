ef81e517a400df20f1a9d0f76ab6367b208fab40
Simon Le Roux <leroux@macbook-air-de-simon-1.home>
2026-05-02 23:44:19 +0200

    chore(release): bump version to 1.1.0

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -188,7 +188,7 @@ export default function Contact() {
             }}
           >
             <span>© 2026 ✽ Simon Le Roux</span>
-            <span>v1.0.4</span>
+            <span>v1.1.0</span>
           </div>
         </FadeIn>
       </div>
